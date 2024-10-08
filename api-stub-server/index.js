08709b530c779cd32a499d57f6ea22ba0e9eef64
naruto1031 <naruto0614@au.com>
2024-10-08 17:59:34 +0900

    fix port

--- api-stub-server/index.js
+++ api-stub-server/index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
     console.log(`helloworld: listening on port ${port}`);
 });
