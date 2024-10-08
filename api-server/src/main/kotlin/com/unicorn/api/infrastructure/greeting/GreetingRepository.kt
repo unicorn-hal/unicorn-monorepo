3052d1c9a79a8d51872d95618c4a33a6117d8ed0
naruto1031 <naruto0614@au.com>
2024-10-08 10:34:56 +0900

    feat: update greeting api

--- api-server/src/main/kotlin/com/unicorn/api/infrastructure/greeting/GreetingRepository.kt
+++ api-server/src/main/kotlin/com/unicorn/api/infrastructure/greeting/GreetingRepository.kt
@@ -4,9 +4,12 @@ import com.unicorn.api.domain.greeting.Greeting
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.util.*
 
 interface GreetingRepository {
     fun store(greeting: Greeting): Greeting
+    fun getOrNullById(id: UUID): Greeting?
+    fun update(greeting: Greeting): Greeting
 }
 
 @Repository
@@ -15,6 +18,46 @@ class GreetingRepositoryImpl(private val namedParameterJdbcTemplate: NamedParame
         val sql = """
             INSERT INTO greeting (id, message)
             VALUES (:id, :message)
+        """.trimIndent()
+
+        val sqlParams = MapSqlParameterSource()
+            .addValue("id", greeting.id)
+            .addValue("message", greeting.message)
+
+        namedParameterJdbcTemplate.update(sql, sqlParams)
+
+        return greeting
+    }
+
+    override fun getOrNullById(id: UUID): Greeting? {
+        val sql = """
+            SELECT
+                id,
+                message
+            FROM greeting
+            WHERE id = :id
+            AND deleted_at IS NULL
+        """.trimIndent()
+
+        val sqlParams = MapSqlParameterSource()
+            .addValue("id", id)
+
+        return namedParameterJdbcTemplate.query(
+            sql,
+            sqlParams
+        ) { rs, _ ->
+           Greeting.fromStore(
+                id = rs.getObject("id", UUID::class.java),
+                message = rs.getString("message")
+           )
+        }.firstOrNull()
+    }
+
+    override fun update(greeting: Greeting): Greeting {
+        val sql = """
+            UPDATE greeting
+            SET message = :message
+            WHERE id = :id
         """.trimIndent()
 
         val sqlParams = MapSqlParameterSource()
