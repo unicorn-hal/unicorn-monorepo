e5d918318ef41936fa7a05a4006430129e7b7922
SadaHiroki <kamannberu0311@gmail.com>
2024-10-18 16:21:46 +0900

    fix: コメント削除

--- api-server/src/test/kotlin/com/unicorn/api/controller/departments/DepartmentsGetTest.kt
+++ api-server/src/test/kotlin/com/unicorn/api/controller/departments/DepartmentsGetTest.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
-// import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -14,7 +13,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.test.context.jdbc.Sql
 
-// @ActiveProfiles("test")
 @TestPropertySource(locations=["classpath:application-test.properties"])
 @SpringBootTest
 @AutoConfigureMockMvc
