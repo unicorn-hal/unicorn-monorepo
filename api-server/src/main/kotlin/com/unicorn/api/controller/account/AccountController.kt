ef6ce2d2268ccc97c32f8de2569f3c01dfb91a48
naruto1031 <naruto0614@au.com>
2024-10-20 12:18:05 +0900

    feat: Account取得API実装

--- api-server/src/main/kotlin/com/unicorn/api/controller/account/AccountController.kt
+++ api-server/src/main/kotlin/com/unicorn/api/controller/account/AccountController.kt
@@ -2,8 +2,11 @@ package com.unicorn.api.controller.account
 
 import com.unicorn.api.application_service.account.SaveAccountService
 import com.unicorn.api.domain.account.Account
+import com.unicorn.api.query_service.account.AccountDto
+import com.unicorn.api.query_service.account.AccountQueryService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader
 @Controller
 class AccountController(
     private val saveAccountService: SaveAccountService,
+    private val accountQueryService: AccountQueryService,
 ) {
     @PostMapping("/accounts")
     fun save(@RequestHeader("X-UID") uid: String, @RequestBody accountPostRequest: AccountPostRequest): ResponseEntity<Account> {
@@ -23,6 +27,18 @@ class AccountController(
             return ResponseEntity.ok(result)
         } catch (e: IllegalArgumentException) {
             return ResponseEntity.badRequest().build()
+        } catch (e: Exception) {
+            return ResponseEntity.internalServerError().build()
+        }
+    }
+
+    @GetMapping("/accounts")
+    fun get(@RequestHeader("X-UID") uid: String): ResponseEntity<AccountDto> {
+        try {
+            val result = accountQueryService.getOrNullBy(uid)
+                ?: return ResponseEntity.notFound().build()
+
+            return ResponseEntity.ok(result)
         } catch (e: Exception) {
             return ResponseEntity.internalServerError().build()
         }
---
+++ api-server/src/test/kotlin/com/unicorn/api/controller/account/AccountGetTest.kt
@@ -0,0 +1,56 @@
+package com.unicorn.api.controller.account
+
+import com.unicorn.api.query_service.account.AccountDto
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
+
+@TestPropertySource(locations = ["classpath:application-test.properties"])
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Sql("/db/account/Insert_Account_Data.sql")
+class AccountGetTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return 200 when account is found`() {
+        val accountDto = AccountDto(
+            uid = "test",
+            role = "user",
+            fcmTokenId = "fcm_token_id"
+        )
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts")
+            .header("X-UID", accountDto.uid))
+
+        result.andExpect(status().isOk)
+        result.andExpect(content().json("""
+            {
+                "uid": "${accountDto.uid}",
+                "role": "${accountDto.role}",
+                "fcmTokenId": "${accountDto.fcmTokenId}"
+            }
+        """.trimIndent()))
+    }
+
+    @Test
+    fun `should return 404 when account is not found`() {
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts")
+            .header("X-UID", "invalid"))
+
+        result.andExpect(status().isNotFound)
+    }
+}
