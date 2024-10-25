022eb6b814aa333ccfb8c6cf0fdd881d761884a3
MasayaIkeno <ikeno.masaya.117@gmail.com>
2024-10-25 23:34:15 +0900

    [TSK-191] おくすり情報更新機能の実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/application_service/medicine/UpdateMedicineService.kt
@@ -0,0 +1,43 @@
+package com.unicorn.api.application_service.medicine
+
+import com.unicorn.api.controller.medicine.MedicinePutRequest
+import com.unicorn.api.domain.medicine.Medicine
+import com.unicorn.api.domain.medicine.MedicineID
+import com.unicorn.api.domain.medicine.MedicineName
+import com.unicorn.api.domain.medicine.Quantity
+import com.unicorn.api.domain.user.*
+import com.unicorn.api.infrastructure.medicine.MedicineRepository
+import com.unicorn.api.infrastructure.user.UserRepository
+import org.springframework.stereotype.Service
+
+interface UpdateMedicineService {
+    fun update(medicineID: MedicineID, userID: UserID, medicinePutRequest: MedicinePutRequest): Medicine
+}
+
+@Service
+class UpdateMedicineServiceImpl(
+    private val userRepository: UserRepository,
+    private val medicineRepository: MedicineRepository
+) : UpdateMedicineService {
+    override fun update(medicineID: MedicineID, userID: UserID, medicinePutRequest: MedicinePutRequest): Medicine {
+        val user = userRepository.getOrNullBy(userID)
+        val medicine = medicineRepository.getOrNullBy(medicineID)
+
+        requireNotNull(user) {
+            "User not found"
+        }
+
+        requireNotNull(medicine) {
+            "Medicine not found"
+        }
+
+        val updateMedicine = medicine.update(
+            medicineName = MedicineName(medicinePutRequest.medicineName),
+            quantity = Quantity(medicinePutRequest.quantity)
+        )
+
+        medicineRepository.store(updateMedicine)
+
+        return updateMedicine
+    }
+}
