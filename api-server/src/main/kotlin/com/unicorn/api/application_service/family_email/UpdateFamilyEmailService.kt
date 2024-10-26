83b817d368030131f42eb793ae54bbd7279548f8
SadaHiroki <kamannberu0311@gmail.com>
2024-10-26 13:17:17 +0900

    feat: family_emailのPUT実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/application_service/family_email/UpdateFamilyEmailService.kt
@@ -0,0 +1,38 @@
+package com.unicorn.api.application_service.family_email
+
+import com.unicorn.api.controller.family_email.FamilyEmailPutRequest
+import com.unicorn.api.domain.family_email.*
+import com.unicorn.api.domain.user.UserID
+import com.unicorn.api.infrastructure.user.UserRepository
+import com.unicorn.api.infrastructure.family_email.FamilyEmailRepository
+import org.springframework.stereotype.Service
+
+interface UpdateFamilyEmailService{
+    fun update(familyEmailID: FamilyEmailID, userID: UserID, familyEmailPutRequest: FamilyEmailPutRequest): FamilyEmail
+}
+
+@Service
+class UpdateFamilyEmailServiceImpl(
+    private val userRepository: UserRepository,
+    private val familyEmailRepository: FamilyEmailRepository
+) : UpdateFamilyEmailService {
+
+    override fun update(familyEmailID: FamilyEmailID, userID: UserID, familyEmailPutRequest: FamilyEmailPutRequest): FamilyEmail {
+        val user = userRepository.getOrNullBy(userID)
+        requireNotNull(user) { "User not found" }
+
+        val familyEmail = familyEmailRepository.getOrNullBy(familyEmailID)
+        requireNotNull(familyEmail) { "Family email not found" }
+
+        val updateFamilyEmail = familyEmail.update(
+            email = Email(familyEmailPutRequest.email),
+            firstName = FirstName(familyEmailPutRequest.firstName),
+            lastName = LastName(familyEmailPutRequest.lastName),
+            phoneNumber = PhoneNumber(familyEmailPutRequest.phoneNumber),
+            iconImageUrl = IconImageUrl(familyEmailPutRequest.iconImageUrl)
+        )
+        familyEmailRepository.store(updateFamilyEmail)
+
+        return updateFamilyEmail
+    }
+}
