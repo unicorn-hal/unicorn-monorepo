a09c74a7794d2d142fb7c106351ea1e4da165c51
naruto1031 <naruto0614@au.com>
2024-10-27 17:48:52 +0900

    feat: Doctor更新APIの実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/application_service/doctor/DoctorUpdateService.kt
@@ -0,0 +1,79 @@
+package com.unicorn.api.application_service.doctor
+
+import com.unicorn.api.controller.doctor.DoctorPutRequest
+import com.unicorn.api.domain.call_support.CallSupportEndHour
+import com.unicorn.api.domain.call_support.CallSupportStartHour
+import com.unicorn.api.domain.chat_support.ChatSupportEndHour
+import com.unicorn.api.domain.chat_support.ChatSupportStartHour
+import com.unicorn.api.domain.department.DepartmentID
+import com.unicorn.api.domain.doctor.*
+import com.unicorn.api.domain.hospital.HospitalID
+import com.unicorn.api.infrastructure.call_support.ChatSupportRepository
+import com.unicorn.api.infrastructure.chat_support.CallSupportRepository
+import com.unicorn.api.infrastructure.department.DepartmentRepository
+import com.unicorn.api.infrastructure.doctor.DoctorRepository
+import com.unicorn.api.infrastructure.hospital.HospitalRepository
+import org.springframework.stereotype.Service
+
+interface DoctorUpdateService {
+    fun update(doctorID: DoctorID, doctorPutRequest: DoctorPutRequest): DoctorPutRequest
+}
+
+@Service
+class DoctorUpdateServiceImpl(
+    private val doctorRepository: DoctorRepository,
+    private val hospitalRepository: HospitalRepository,
+    private val departmentRepository: DepartmentRepository,
+    private val callSupportRepository: CallSupportRepository,
+    private val chatSupportRepository: ChatSupportRepository
+) : DoctorUpdateService {
+    override fun update(doctorID: DoctorID, doctorPutRequest: DoctorPutRequest): DoctorPutRequest {
+        val doctor = doctorRepository.getOrNullBy(doctorID)
+        requireNotNull(doctor) { "Doctor not found" }
+
+        // DB不整合チェック
+        val chatSupport = chatSupportRepository.getOrNullBy(doctor.doctorID)
+        requireNotNull(chatSupport) { "ChatSupport not found" }
+        val callSupport = callSupportRepository.getOrNullBy(doctor.doctorID)
+        requireNotNull(callSupport) { "CallSupport not found" }
+
+        val hospital = hospitalRepository.getOrNullBy(HospitalID(doctorPutRequest.hospitalID))
+        requireNotNull(hospital) { "Hospital not found" }
+
+        val departmentIDs = doctorPutRequest.departments.map { DepartmentID(it) }
+        val departments = departmentRepository.findByDepartmentIDs(departmentIDs)
+        val missingDepartmentIDs = departmentIDs - departments.map { it?.departmentID }.toSet()
+        //　存在しないdepartmentIDがある場合はエラー
+        require(missingDepartmentIDs.isEmpty()) {
+            val response = missingDepartmentIDs.joinToString(
+                separator = ", ",
+                transform = { it?.value.toString() }
+            )
+            "Department not found: $response"
+        }
+
+        val updatedDoctor = doctor.update(
+            hospitalID = hospital.hospitalID,
+            email = Email(doctorPutRequest.email),
+            phoneNumber = PhoneNumber(doctorPutRequest.phoneNumber),
+            firstName = FirstName(doctorPutRequest.firstName),
+            lastName = LastName(doctorPutRequest.lastName),
+            doctorIconUrl = doctorPutRequest.doctorIconUrl?.let { DoctorIconUrl(it) },
+            departments = departmentIDs,
+        )
+        val updatedChatSupport = chatSupport.update(
+            chatSupportStartHour = ChatSupportStartHour(doctorPutRequest.chatSupportStartHour),
+            chatSupportEndHour = ChatSupportEndHour(doctorPutRequest.chatSupportEndHour)
+        )
+        val updatedCallSupport = callSupport.update(
+            callSupportStartHour = CallSupportStartHour(doctorPutRequest.callSupportStartHour),
+            callSupportEndHour = CallSupportEndHour(doctorPutRequest.callSupportEndHour)
+        )
+
+        doctorRepository.store(updatedDoctor)
+        chatSupportRepository.store(updatedChatSupport)
+        callSupportRepository.store(updatedCallSupport)
+
+        return doctorPutRequest
+    }
+}
--- api-server/src/main/kotlin/com/unicorn/api/controller/doctor/DoctorController.kt
+++ api-server/src/main/kotlin/com/unicorn/api/controller/doctor/DoctorController.kt
@@ -2,11 +2,15 @@ package com.unicorn.api.controller.doctor
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.unicorn.api.application_service.doctor.DoctorSaveService
+import com.unicorn.api.application_service.doctor.DoctorUpdateService
 import com.unicorn.api.controller.api_response.ResponseError
 import com.unicorn.api.domain.account.UID
+import com.unicorn.api.domain.doctor.DoctorID
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import java.time.LocalTime
@@ -14,22 +18,60 @@ import java.util.*
 
 @Controller
 class DoctorController(
-    private val doctorSaveService: DoctorSaveService
+    private val doctorSaveService: DoctorSaveService,
+    private val doctorUpdateService: DoctorUpdateService
 ) {
     @PostMapping("/doctors")
-    fun post(@RequestHeader("X-UID") uid: String, @RequestBody doctorPostRequest: DoctorPostRequest): ResponseEntity<*> {
+    fun post(
+        @RequestHeader("X-UID") uid: String,
+        @RequestBody doctorPostRequest: DoctorPostRequest
+    ): ResponseEntity<*> {
         try {
             val result = doctorSaveService.save(UID(uid), doctorPostRequest)
             return ResponseEntity.ok(result)
         } catch (e: IllegalArgumentException) {
             return ResponseEntity.badRequest().body(ResponseError(e.message ?: "Bad Request"))
         } catch (e: Exception) {
-            return ResponseEntity.internalServerError().body(ResponseError( "Internal Server Error"))
+            return ResponseEntity.internalServerError().body(ResponseError("Internal Server Error"))
+        }
+    }
+
+    @PutMapping("/doctors/{doctorID}")
+    fun put(
+        @RequestHeader("X-UID") uid: String,
+        @PathVariable doctorID: String,
+        @RequestBody doctorPutRequest: DoctorPutRequest
+    ): ResponseEntity<*> {
+        try {
+            val result = doctorUpdateService.update(DoctorID(doctorID), doctorPutRequest)
+            return ResponseEntity.ok(result)
+        } catch (e: IllegalArgumentException) {
+            return ResponseEntity.badRequest().body(ResponseError(e.message ?: "Bad Request"))
+        } catch (e: Exception) {
+            return ResponseEntity.internalServerError().body(ResponseError("Internal Server Error"))
         }
     }
 }
 
 data class DoctorPostRequest(
+    val hospitalID: UUID,
+    val email: String,
+    val phoneNumber: String,
+    val firstName: String,
+    val lastName: String,
+    val doctorIconUrl: String?,
+    val departments: List<UUID>,
+    @JsonFormat(pattern = "HH:mm")
+    val chatSupportStartHour: LocalTime,
+    @JsonFormat(pattern = "HH:mm")
+    val chatSupportEndHour: LocalTime,
+    @JsonFormat(pattern = "HH:mm")
+    val callSupportStartHour: LocalTime,
+    @JsonFormat(pattern = "HH:mm")
+    val callSupportEndHour: LocalTime,
+)
+
+data class DoctorPutRequest(
     val hospitalID: UUID,
     val email: String,
     val phoneNumber: String,
