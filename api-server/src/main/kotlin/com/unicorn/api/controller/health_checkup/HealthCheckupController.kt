2e304f59fb13c1ea05f439702fa8a2457902aa92
SadaHiroki <kamannberu0311@gmail.com>
2024-11-01 16:30:33 +0900

    feat: health_checkupのIDごとのget機能実装

--- api-server/src/main/kotlin/com/unicorn/api/controller/health_checkup/HealthCheckupController.kt
+++ api-server/src/main/kotlin/com/unicorn/api/controller/health_checkup/HealthCheckupController.kt
@@ -5,6 +5,8 @@ import com.unicorn.api.application_service.health_checkup.*
 import com.unicorn.api.controller.api_response.ResponseError
 import com.unicorn.api.domain.health_checkup.HealthCheckupID
 import com.unicorn.api.domain.user.UserID
+import com.unicorn.api.query_service.health_checkup.HealthCheckupQueryService
+import com.unicorn.api.query_service.user.UserQueryService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -13,9 +15,11 @@ import java.util.*
 
 @Controller
 class HealthCheckupController(
+    private val userQueryService: UserQueryService,
     private val saveHealthCheckupService: SaveHealthCheckupService,
     private val updateHealthCheckupService: UpdateHealthCheckupService,
     private val deleteHealthCheckupService: DeleteHealthCheckupService,
+    private val healthCheckupQueryService: HealthCheckupQueryService,
 ) {
     @PostMapping("/health_checkups")
     fun post(
@@ -27,6 +31,24 @@ class HealthCheckupController(
             return ResponseEntity.ok(result)
         } catch (e: IllegalArgumentException) {
             return ResponseEntity.badRequest().body(ResponseError(e.message ?: "Bad Request"))
+        } catch (e: Exception) {
+            return ResponseEntity.internalServerError().body(ResponseError("Internal Server Error"))
+        }
+    }
+
+    @GetMapping("/health_checkups/{healthCheckupID}")
+    fun get(
+        @RequestHeader("X-UID") uid: String,
+        @PathVariable healthCheckupID: UUID,
+    ): ResponseEntity<*> {
+        try {
+            userQueryService.getOrNullBy(uid)
+                ?: return ResponseEntity.badRequest().body(ResponseError("User not found"))
+
+            val result =
+                healthCheckupQueryService.getOrNullBy(HealthCheckupID(healthCheckupID))
+                    ?: return ResponseEntity.badRequest().body(ResponseError("Health checkup not found"))
+            return ResponseEntity.ok(result)
         } catch (e: Exception) {
             return ResponseEntity.internalServerError().body(ResponseError("Internal Server Error"))
         }
---
+++ api-server/src/main/kotlin/com/unicorn/api/query_service/health_checkup/HealthCheckupQueryService.kt
@@ -0,0 +1,99 @@
+package com.unicorn.api.query_service.health_checkup
+
+import com.unicorn.api.domain.health_checkup.HealthCheckupID
+import com.unicorn.api.domain.user.UserID
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.util.*
+
+interface HealthCheckupQueryService {
+    fun getBy(userID: UserID): HealthCheckupResult
+
+    fun getOrNullBy(healthcheckupID: HealthCheckupID): HealthCheckupDto?
+}
+
+@Service
+class HealthCheckupQueryServiceImpl(
+    private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate,
+) : HealthCheckupQueryService {
+    override fun getBy(userID: UserID): HealthCheckupResult {
+        val sql =
+            """
+            SELECT
+                health_checkup_id,
+                checkuped_user_id,
+                body_temperature,
+                blood_pressure,
+                medical_record,
+                checkuped_date
+            FROM health_checkups
+            WHERE checkuped_user_id = :userID
+            AND deleted_at IS NULL
+            """.trimIndent()
+
+        val sqlParams =
+            MapSqlParameterSource()
+                .addValue("userID", userID.value)
+        val healthCheckups =
+            namedParameterJdbcTemplate.query(
+                sql,
+                sqlParams,
+            ) { rs, _ ->
+                HealthCheckupDto(
+                    healthCheckupID = UUID.fromString(rs.getString("health_checkup_id")),
+                    userID = rs.getString("checkuped_user_id"),
+                    bodyTemperature = rs.getDouble("body_temperature"),
+                    bloodPressure = rs.getString("blood_pressure"),
+                    medicalRecord = rs.getString("medical_record"),
+                    date = rs.getDate("checkuped_date").toLocalDate(),
+                )
+            }
+        return HealthCheckupResult(healthCheckups)
+    }
+
+    override fun getOrNullBy(healthcheckupID: HealthCheckupID): HealthCheckupDto? {
+        val sql =
+            """
+            SELECT
+                health_checkup_id,
+                checkuped_user_id,
+                body_temperature,
+                blood_pressure,
+                medical_record,
+                checkuped_date
+            FROM health_checkups
+            WHERE deleted_at IS NULL 
+            AND health_checkup_id = :health_checkup_id
+            """.trimIndent()
+
+        val params =
+            MapSqlParameterSource()
+                .addValue("health_checkup_id", healthcheckupID.value)
+
+        return namedParameterJdbcTemplate.query(sql, params) { rs, _ ->
+            HealthCheckupDto(
+                healthCheckupID = UUID.fromString(rs.getString("health_checkup_id")),
+                userID = rs.getString("checkuped_user_id"),
+                bodyTemperature = rs.getDouble("body_temperature"),
+                bloodPressure = rs.getString("blood_pressure"),
+                medicalRecord = rs.getString("medical_record"),
+                date = rs.getDate("checkuped_date").toLocalDate(),
+            )
+        }.singleOrNull()
+    }
+}
+
+data class HealthCheckupDto(
+    val healthCheckupID: UUID,
+    val userID: String,
+    val bodyTemperature: Double,
+    val bloodPressure: String,
+    val medicalRecord: String,
+    val date: LocalDate,
+)
+
+data class HealthCheckupResult(
+    val healthCheckups: List<HealthCheckupDto>,
+)
