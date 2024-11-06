3f58d5e0d05fbd661d2a186391d3fe334441bba4
MasayaIkeno <ikeno.masaya.117@gmail.com>
2024-11-06 15:43:45 +0900

    [TSK-167] primary_doctors 登録機能の実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/application_service/primary_doctor/SavePrimaryDoctorService.kt
@@ -0,0 +1,49 @@
+package com.unicorn.api.application_service.primary_doctor
+
+import com.unicorn.api.controller.primary_doctor.PrimaryDoctorPostRequest
+import com.unicorn.api.domain.account.UID
+import com.unicorn.api.domain.doctor.DoctorID
+import com.unicorn.api.domain.primary_doctor.PrimaryDoctors
+import com.unicorn.api.domain.user.UserID
+import com.unicorn.api.infrastructure.account.AccountRepository
+import com.unicorn.api.infrastructure.doctor.DoctorRepository
+import com.unicorn.api.infrastructure.primary_doctor.PrimaryDoctorRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+interface SavePrimaryDoctorService {
+    fun save(uid: String, primaryDoctorPostRequest: PrimaryDoctorPostRequest): PrimaryDoctors
+}
+
+@Service
+class SavePrimaryDoctorServiceImpl(
+    private val accountRepository: AccountRepository,
+    private val doctorRepository: DoctorRepository,
+    private val primaryDoctorRepository: PrimaryDoctorRepository
+) : SavePrimaryDoctorService {
+    @Transactional
+    override fun save(uid: String, primaryDoctorPostRequest: PrimaryDoctorPostRequest): PrimaryDoctors {
+        val account = accountRepository.getOrNullByUid(UID(uid))
+        requireNotNull(account) { "Account not found" }
+        require(account.isUser()) { "Account is not user" }
+
+
+        // ここ、doctorID を使用して
+        // Doctor の存在確認をしようとしましたが上手くいかないです...
+//        val doctorIDList = primaryDoctorPostRequest.doctorIDs.map { doctorID ->
+//            doctorRepository.getOrNullBy(DoctorID(doctorID))
+//                ?: throw IllegalArgumentException("Doctor not found for ID: $doctorID")
+//            DoctorID(doctorID)
+//        }
+        val doctorIDList = primaryDoctorPostRequest.doctorIDs.map { DoctorID(it) }
+
+        val primaryDoctors = PrimaryDoctors.create(
+            userID = UserID(uid),
+            doctorIDs = doctorIDList
+        )
+
+        primaryDoctorRepository.store(primaryDoctors)
+
+        return primaryDoctors
+    }
+}
---
+++ api-server/src/test/kotlin/com/unicorn/api/controller/primary_doctor/PrimaryDoctorPostTest.kt
@@ -0,0 +1,140 @@
+package com.unicorn.api.controller.primary_doctor
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.unicorn.api.controller.medicine.MedicinePostRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
+import kotlin.test.Test
+
+@TestPropertySource(locations = ["classpath:application-test.properties"])
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+@Sql("/db/primary_doctor/Insert_Parent_Account_Data.sql")
+@Sql("/db/primary_doctor/Insert_User_Data.sql")
+@Sql("/db/primary_doctor/Insert_Hospital_Data.sql")
+@Sql("/db/primary_doctor/Insert_Department_Data.sql")
+@Sql("/db/primary_doctor/Insert_Doctor_Data.sql")
+@Sql("/db/primary_doctor/Insert_Doctor_Department_Data.sql")
+@Sql("/db/primary_doctor/Insert_PrimaryDoctor_Data.sql")
+class PrimaryDoctorPostTest {
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return 200 when primary doctor is created`() {
+        val userID = "12345"
+        val doctorIDs = listOf("doctor5")
+        val primaryDoctorIDs = PrimaryDoctorPostRequest(
+            doctorIDs = doctorIDs
+        )
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.post("/primary_doctors").headers(HttpHeaders().apply {
+                add("X-UID", userID)
+            })
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(primaryDoctorIDs))
+        )
+
+        result.andExpect(status().isOk)
+        result.andExpect(
+            content().json(
+                """
+                    {
+                        "userID": "12345",
+                        "doctorIDs": [
+                            "doctor5"
+                        ]
+                    }
+                """.trimIndent()
+            )
+        )
+    }
+
+    @Test
+    fun `should return 200 when multiple primary doctors are created`() {
+        val userID = "12345"
+        val doctorIDs = listOf("doctor5", "doctor6")
+        val primaryDoctorIDs = PrimaryDoctorPostRequest(
+            doctorIDs = doctorIDs
+        )
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.post("/primary_doctors").headers(HttpHeaders().apply {
+                add("X-UID", userID)
+            })
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(primaryDoctorIDs))
+        )
+
+        result.andExpect(status().isOk)
+        result.andExpect(
+            content().json(
+                """
+                    {
+                        "userID": "12345",
+                        "doctorIDs": [
+                            "doctor5",
+                            "doctor6"
+                        ]
+                    }
+                """.trimIndent()
+            )
+        )
+    }
+
+    @Test
+    fun `should return 400 when doctorID is missing`() {
+        val userID = "12345"
+        val doctorIDs = listOf("")
+        val primaryDoctorPostRequest = PrimaryDoctorPostRequest(
+            doctorIDs = doctorIDs
+        )
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.post("/primary_doctors").headers(HttpHeaders().apply {
+                add("X-UID", userID)
+            })
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(primaryDoctorPostRequest))
+        )
+
+        result.andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `should return 400 when account is a doctor`() {
+        val userID = "doctor"
+        val doctorIDs = listOf("doctor6")
+        val primaryDoctorPostRequest = PrimaryDoctorPostRequest(
+            doctorIDs = doctorIDs
+        )
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.post("/primary_doctors").headers(HttpHeaders().apply {
+                add("X-UID", userID)
+            })
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(primaryDoctorPostRequest))
+        )
+
+        result.andExpect(status().isBadRequest)
+    }
+}
