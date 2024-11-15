7747d7c63627454651ef4bfae8afcc13888f3363
lenny <mclenny817@gmail.com>
2024-11-16 00:16:01 +0900

    add : ドメイン層のupdateテストの実装

--- api-server/src/main/kotlin/com/unicorn/api/domain/call/Call.kt
+++ api-server/src/main/kotlin/com/unicorn/api/domain/call/Call.kt
@@ -1,61 +1,35 @@
 package com.unicorn.api.domain.call
 
-import com.unicorn.api.domain.doctor.DoctorID
-import com.unicorn.api.domain.user.UserID
 import java.time.OffsetDateTime
-import java.util.UUID
+import java.util.*
 
-data class Call
-    private constructor(
-        val callReservationID: CallReservationID,
-        val doctorID: DoctorID,
-        val userID: UserID,
-        val callStartTime: CallStartTime,
-        val callEndTime: CallEndTime,
-    ) {
-        companion object {
-            fun fromStore(
-                callReservationID: UUID,
-                doctorID: String,
-                userID: String,
-                callStartTime: OffsetDateTime,
-                callEndTime: OffsetDateTime,
-            ): Call {
-                return Call(
-                    callReservationID = CallReservationID(callReservationID),
-                    doctorID = DoctorID(doctorID),
-                    userID = UserID(userID),
-                    callStartTime = CallStartTime(callStartTime),
-                    callEndTime = CallEndTime(callEndTime),
-                )
-            }
-
-            fun create(
-                doctorID: String,
-                userID: String,
-                callStartTime: OffsetDateTime,
-                callEndTime: OffsetDateTime,
-            ): Call {
-                val startTime = CallStartTime(callStartTime)
-                val endTime = CallEndTime(callEndTime)
-
-                require(startTime.value.isBefore(endTime.value)) {
-                    "The call start time must be before the end time."
-                }
-
-                return Call(
-                    callReservationID = CallReservationID(UUID.randomUUID()),
-                    doctorID = DoctorID(doctorID),
-                    userID = UserID(userID),
-                    callStartTime = startTime,
-                    callEndTime = endTime,
-                )
-            }
+data class Call private constructor(
+    val callReservationID: CallReservationID,
+    val doctorID: DoctorID,
+    val userID: UserID,
+    val callStartTime: CallStartTime,
+    val callEndTime: CallEndTime,
+) {
+    companion object {
+        fun fromStore(
+            callReservationID: UUID,
+            doctorID: String,
+            userID: String,
+            callStartTime: OffsetDateTime,
+            callEndTime: OffsetDateTime,
+        ): Call {
+            return Call(
+                callReservationID = CallReservationID(callReservationID),
+                doctorID = DoctorID(doctorID),
+                userID = UserID(userID),
+                callStartTime = CallStartTime(callStartTime),
+                callEndTime = CallEndTime(callEndTime),
+            )
         }
 
-        fun update(
-            doctorID: DoctorID,
-            userID: UserID,
+        fun create(
+            doctorID: String,
+            userID: String,
             callStartTime: OffsetDateTime,
             callEndTime: OffsetDateTime,
         ): Call {
@@ -66,18 +40,47 @@ data class Call
                 "The call start time must be before the end time."
             }
 
-            return this.copy(
-                callReservationID = callReservationID,
-                doctorID = doctorID,
-                userID = userID,
+            return Call(
+                callReservationID = CallReservationID(UUID.randomUUID()),
+                doctorID = DoctorID(doctorID),
+                userID = UserID(userID),
                 callStartTime = startTime,
                 callEndTime = endTime,
             )
         }
     }
 
+    fun update(
+        doctorID: DoctorID,
+        userID: UserID,
+        callStartTime: OffsetDateTime,
+        callEndTime: OffsetDateTime,
+    ): Call {
+        val startTime = CallStartTime(callStartTime)
+        val endTime = CallEndTime(callEndTime)
+
+        require(startTime.value.isBefore(endTime.value)) {
+            "The call start time must be before the end time."
+        }
+
+        return this.copy(
+            callReservationID = callReservationID,
+            doctorID = doctorID,
+            userID = userID,
+            callStartTime = startTime,
+            callEndTime = endTime,
+        )
+    }
+}
+
 @JvmInline
 value class CallReservationID(val value: UUID)
+
+@JvmInline
+value class DoctorID(val value: String)
+
+@JvmInline
+value class UserID(val value: String)
 
 @JvmInline
 value class CallStartTime(val value: OffsetDateTime)
--- api-server/src/test/kotlin/com/unicorn/api/domain/call/CallTest.kt
+++ api-server/src/test/kotlin/com/unicorn/api/domain/call/CallTest.kt
@@ -27,15 +27,13 @@ class CallTest {
 
     @Test
     fun `should not create callReservation with invalid time`() {
-        val callStartTime = OffsetDateTime.now().plusDays(1)
-        val callEndTime = OffsetDateTime.now()
         val exception =
             assertThrows(IllegalArgumentException::class.java) {
                 Call.create(
                     doctorID = "12345",
                     userID = "12345",
-                    callStartTime = callStartTime,
-                    callEndTime = callEndTime,
+                    callStartTime = callEndTime,
+                    callEndTime = callStartTime,
                 )
             }
         assertEquals("The call start time must be before the end time.", exception.message)
@@ -56,5 +54,29 @@ class CallTest {
         assertEquals("12345", call.userID.value)
         assertEquals(callStartTime, call.callStartTime.value)
         assertEquals(callEndTime, call.callEndTime.value)
+    }
+
+    @Test
+    fun `should update callReservation`() {
+        val call =
+            Call.create(
+                doctorID = "12345",
+                userID = "12345",
+                callStartTime = callStartTime,
+                callEndTime = callEndTime,
+            )
+
+        val updatedCall =
+            call.update(
+                doctorID = DoctorID("67890"),
+                userID = UserID("67890"),
+                callStartTime = callStartTime.plusDays(1),
+                callEndTime = callEndTime.plusDays(1),
+            )
+
+        assertEquals("67890", updatedCall.doctorID.value)
+        assertEquals("67890", updatedCall.userID.value)
+        assertEquals(callStartTime.plusDays(1), updatedCall.callStartTime.value)
+        assertEquals(callEndTime.plusDays(1), updatedCall.callEndTime.value)
     }
 }
