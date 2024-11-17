882f143fa48252bc0f54211e9afd0a5791a81b06
SadaHiroki <kamannberu0311@gmail.com>
2024-11-17 18:43:50 +0900

    feat: 通知APIの登録実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/controller/notification/NotificationController.kt
@@ -0,0 +1,35 @@
+package com.unicorn.api.controller.notification
+
+import com.unicorn.api.application_service.notification.*
+import com.unicorn.api.controller.api_response.ResponseError
+import com.unicorn.api.domain.user.UserID
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+
+@Controller
+class NotificationController(
+    private val saveNotificationService: SaveNotificationService,
+) {
+    @PostMapping("/users/{userID}/notification")
+    fun post(
+        @RequestHeader("X-UID") uid: String,
+        @PathVariable userID: String,
+        @RequestBody notificationPostRequest: NotificationPostRequest,
+    ): ResponseEntity<*> {
+        try {
+            val result = saveNotificationService.save(UserID(userID), notificationPostRequest)
+            return ResponseEntity.ok(result)
+        } catch (e: IllegalArgumentException) {
+            return ResponseEntity.status(400).body(ResponseError(e.message ?: "Bad Request"))
+        } catch (e: Exception) {
+            return ResponseEntity.status(500).body(ResponseError("Internal Server Error"))
+        }
+    }
+}
+
+data class NotificationPostRequest(
+    val isMedicineReminder: Boolean,
+    val isRegularHealthCheckup: Boolean,
+    val isHospitalNews: Boolean,
+)
