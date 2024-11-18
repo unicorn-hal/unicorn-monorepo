b0f90ef71757f51ab772f858104d74691d42b9b5
SadaHiroki <kamannberu0311@gmail.com>
2024-11-18 12:28:21 +0900

    feat: 通知APIの取得実装

--- api-server/src/main/kotlin/com/unicorn/api/controller/notification/NotificationController.kt
+++ api-server/src/main/kotlin/com/unicorn/api/controller/notification/NotificationController.kt
@@ -3,6 +3,8 @@ package com.unicorn.api.controller.notification
 import com.unicorn.api.application_service.notification.*
 import com.unicorn.api.controller.api_response.ResponseError
 import com.unicorn.api.domain.user.UserID
+import com.unicorn.api.query_service.notification.NotificationQueryService
+import com.unicorn.api.query_service.user.UserQueryService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -11,7 +13,27 @@ import org.springframework.web.bind.annotation.*
 class NotificationController(
     private val saveNotificationService: SaveNotificationService,
     private val updateNotificationService: UpdateNotificationService,
+    private val notificationQueryService: NotificationQueryService,
+    private val userQueryService: UserQueryService,
 ) {
+    @GetMapping("/users/{userID}/notification")
+    fun get(
+        @RequestHeader("X-UID") uid: String,
+        @PathVariable userID: String,
+    ): ResponseEntity<*> {
+        try {
+            userQueryService.getOrNullBy(userID)
+                ?: return ResponseEntity.status(400).body(ResponseError("User not found"))
+
+            val result =
+                notificationQueryService.getOrNullBy(UserID(userID))
+                    ?: return ResponseEntity.status(404).body(ResponseError("Notification not found"))
+            return ResponseEntity.ok(result)
+        } catch (e: Exception) {
+            return ResponseEntity.status(500).body(ResponseError("Internal Server Error"))
+        }
+    }
+
     @PostMapping("/users/{userID}/notification")
     fun post(
         @RequestHeader("X-UID") uid: String,
