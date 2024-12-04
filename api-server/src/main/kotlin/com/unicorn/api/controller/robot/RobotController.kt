3c8ea795c323c4677c4103c879f0318b0acf10b5
naruto1031 <naruto0614@au.com>
2024-12-04 22:02:30 +0900

    feat: robot delete api実装

--- api-server/src/main/kotlin/com/unicorn/api/controller/robot/RobotController.kt
+++ api-server/src/main/kotlin/com/unicorn/api/controller/robot/RobotController.kt
@@ -1,5 +1,6 @@
 package com.unicorn.api.controller.robot
 
+import com.unicorn.api.application_service.robot.DeleteRobotService
 import com.unicorn.api.application_service.robot.SaveRobotService
 import com.unicorn.api.application_service.robot.UpdateRobotService
 import com.unicorn.api.controller.api_response.ResponseError
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*
 class RobotController(
     private val saveRobotService: SaveRobotService,
     private val updateRobotService: UpdateRobotService,
+    private val deleteRobotService: DeleteRobotService,
 ) {
     @PostMapping("/robots")
     fun save(
@@ -38,6 +40,21 @@ class RobotController(
         try {
             val result = updateRobotService.update(RobotID(robotID), updateRobotRequest)
             return ResponseEntity.ok(result)
+        } catch (e: IllegalArgumentException) {
+            return ResponseEntity.badRequest().body(ResponseError(e.message ?: "Bad Request"))
+        } catch (e: Exception) {
+            return ResponseEntity.internalServerError().body("Internal Server Error")
+        }
+    }
+
+    @DeleteMapping("/robots/{robotID}")
+    fun delete(
+        @RequestHeader("X-UID") uid: String,
+        @PathVariable robotID: String,
+    ): ResponseEntity<Any> {
+        try {
+            deleteRobotService.delete(RobotID(robotID))
+            return ResponseEntity.noContent().build()
         } catch (e: IllegalArgumentException) {
             return ResponseEntity.badRequest().body(ResponseError(e.message ?: "Bad Request"))
         } catch (e: Exception) {
