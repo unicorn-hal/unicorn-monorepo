96e76f978df3c2ea072f3d8f62f200b040f2a88b
SadaHiroki <kamannberu0311@gmail.com>
2024-12-09 10:53:09 +0900

    feat: 完了通知API実装

---
+++ api-server/src/main/kotlin/com/unicorn/api/application_service/complete/NotifyCompleteService.kt
@@ -0,0 +1,58 @@
+package com.unicorn.api.application_service.complete
+
+import com.unicorn.api.controller.complete.CompletePostRequest
+import com.unicorn.api.domain.emergency.EmergencySavedEvent
+import com.unicorn.api.domain.robot.RobotID
+import com.unicorn.api.domain.robot.RobotStatus
+import com.unicorn.api.domain.robot_support.RobotSupportID
+import com.unicorn.api.domain.unicorn_status.EmergencyUserStatus
+import com.unicorn.api.infrastructure.emergency.EmergencyRepository
+import com.unicorn.api.infrastructure.robot.RobotRepository
+import com.unicorn.api.infrastructure.robot_support.RobotSupportRepository
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Service
+import java.util.*
+
+interface NotifyCompleteService {
+    fun notify(
+        robotID: RobotID,
+        completePostRequest: CompletePostRequest,
+    ): EmergencyUserStatus
+}
+
+@Service
+class NotifyCompleteServiceImpl(
+    private val robotRepository: RobotRepository,
+    private val emergencyRepository: EmergencyRepository,
+    private val robotSupportRepository: RobotSupportRepository,
+    private val applicationEventPublisher: ApplicationEventPublisher,
+) : NotifyCompleteService {
+    override fun notify(
+        robotID: RobotID,
+        completePostRequest: CompletePostRequest,
+    ): EmergencyUserStatus {
+        val robot = robotRepository.getOrNullBy(robotID)
+        requireNotNull(robot) { "Robot not found" }
+
+        val robotSupportID = RobotSupportID(completePostRequest.robotSupportID)
+        val robotSupport = robotSupportRepository.getOrNull(robotSupportID)
+        requireNotNull(robotSupport) { "Robot support not found" }
+
+        robotSupportRepository.delete(robotSupport)
+
+        val waitingRobot = robot.updateStatus(RobotStatus.robot_waiting)
+        robotRepository.store(waitingRobot)
+
+        val emergencyUserStatus =
+            EmergencyUserStatus.complete(
+                robotID = robotID.value,
+                robotName = robot.robotName.value,
+            )
+
+        val waitingEmergencyList = emergencyRepository.getOlderOrNull()
+        for (waitingEmergency in waitingEmergencyList) {
+            applicationEventPublisher.publishEvent(EmergencySavedEvent(waitingEmergency))
+        }
+        return emergencyUserStatus
+    }
+}
---
+++ api-server/src/test/kotlin/com/unicorn/api/controller/complete/PostCompleteTest.kt
@@ -0,0 +1,76 @@
+package com.unicorn.api.controller.complete
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@TestPropertySource(locations = ["classpath:application-test.properties"])
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureMockMvc
+@Transactional
+@Sql("/db/robot_support/Insert_Parent_Account_Data.sql")
+@Sql("/db/user/Insert_User_Data.sql")
+@Sql("/db/robot_support/Insert_Robot_Data.sql")
+@Sql("/db/emergency/Insert_Emergency_Data.sql")
+@Sql("/db/robot_support/Insert_Robot_Support_Data.sql")
+class PostCompleteTest {
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return 200 when complete`() {
+        val completePostRequest =
+            CompletePostRequest(
+                robotSupportID = UUID.fromString("c64e788c-dd0a-72d8-a271-5460e1f29816"),
+                userID = "test",
+            )
+
+        val robotID = "robot"
+
+        val result =
+            mockMvc.perform(
+                MockMvcRequestBuilders.post("/unicorn/$robotID/complete").headers(
+                    HttpHeaders().apply {
+                        add("X-UID", "robot")
+                    },
+                )
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(completePostRequest)),
+            )
+
+        result.andExpect(status().isOk)
+        result.andExpect(
+            content().json(
+                """
+                {
+                    "robotID": "robot",
+                    "robotName": "robotName",
+                    "status": "complete",
+                    "waitingNumber": null,
+                    "robotLatitude": null,
+                    "robotLongitude": null
+                }
+                """.trimIndent(),
+                true,
+            ),
+        )
+    }
+}
