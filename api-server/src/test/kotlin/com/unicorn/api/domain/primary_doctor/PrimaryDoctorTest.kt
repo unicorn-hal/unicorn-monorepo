e46955367c6733728329182636bd2d05d1f84916
SadaHiroki <kamannberu0311@gmail.com>
2024-12-10 15:45:37 +0900

    feat: かかりつけ医の改修

--- api-server/src/test/kotlin/com/unicorn/api/domain/primary_doctor/PrimaryDoctorTest.kt
+++ api-server/src/test/kotlin/com/unicorn/api/domain/primary_doctor/PrimaryDoctorTest.kt
@@ -1,91 +1,33 @@
 package com.unicorn.api.domain.primary_doctor
 
-import com.unicorn.api.domain.doctor.DoctorID
-import com.unicorn.api.domain.user.UserID
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import java.util.*
 
-class PrimaryDoctorsTest {
+class PrimaryDoctorTest {
     @Test
-    fun `create PrimaryDoctors with initial doctors`() {
-        val userID = UserID("test")
-        val doctorID = DoctorID("doctor")
+    fun `should create primary doctor`() {
+        val primaryDoctor =
+            PrimaryDoctor.create(
+                userID = "test",
+                doctorID = "doctor",
+            )
 
-        val primaryDoctors = PrimaryDoctors.create(userID, listOf(doctorID))
-
-        assertNotNull(primaryDoctors)
-        assertEquals(1, primaryDoctors.doctors.size)
-        assertEquals(doctorID, primaryDoctors.doctors[0].doctorID)
+        assertEquals("test", primaryDoctor.userID.value)
+        assertEquals("doctor", primaryDoctor.doctorID.value)
     }
 
     @Test
-    fun `add a new doctor using fromStore`() {
-        val userID = UserID("test")
-        val initialDoctorID = DoctorID("doctor")
+    fun `should create primary doctor from store`() {
+        val primaryDoctor =
+            PrimaryDoctor.fromStore(
+                primaryDoctorID = UUID.fromString("d8bfa31d-54b9-4c64-a499-6c522517e5a0"),
+                userID = "test",
+                doctorID = "doctor",
+            )
 
-        var primaryDoctors = PrimaryDoctors.create(userID, listOf(initialDoctorID))
-
-        val newDoctorID = DoctorID("doctor2")
-        primaryDoctors = PrimaryDoctors.fromStore(userID, primaryDoctors.doctors, newDoctorID)
-
-        assertEquals(2, primaryDoctors.doctors.size)
-        assertEquals(initialDoctorID, primaryDoctors.doctors[0].doctorID)
-        assertEquals(newDoctorID, primaryDoctors.doctors[1].doctorID)
-    }
-
-    @Test
-    fun `update doctors list`() {
-        val userID = UserID("test")
-        val initialDoctorID = DoctorID("doctor")
-        val primaryDoctors = PrimaryDoctors.create(userID, listOf(initialDoctorID))
-
-        val newDoctorID = DoctorID("doctor2")
-        val newDoctorIDs = listOf(newDoctorID)
-        val updatePrimaryDoctors = primaryDoctors.updateDoctors(newDoctorIDs)
-
-        assertEquals(1, updatePrimaryDoctors.doctors.size)
-    }
-
-    @Test
-    fun `update doctors with no common doctors between existing and new list`() {
-        val userID = UserID("test")
-        val existingDoctorID1 = DoctorID("doctor1")
-        val existingDoctorID2 = DoctorID("doctor2")
-        val primaryDoctors = PrimaryDoctors.create(userID, listOf(existingDoctorID1, existingDoctorID2))
-
-        // newDoctorIDsには存在しないドクターIDを指定
-        val newDoctorIDs = listOf(DoctorID("doctor3"), DoctorID("doctor4"))
-        val updatePrimaryDoctors = primaryDoctors.updateDoctors(newDoctorIDs)
-
-        assertEquals(2, updatePrimaryDoctors.doctors.size)
-    }
-
-    @Test
-    fun `update doctors with all existing doctors in new list`() {
-        val userID = UserID("test")
-        val existingDoctorID1 = DoctorID("doctor1")
-        val existingDoctorID2 = DoctorID("doctor2")
-        val primaryDoctors = PrimaryDoctors.create(userID, listOf(existingDoctorID1, existingDoctorID2))
-
-        // newDoctorIDsに既存のドクターIDをすべて指定
-        val newDoctorIDs = listOf(existingDoctorID1, existingDoctorID2)
-        val updatePrimaryDoctors = primaryDoctors.updateDoctors(newDoctorIDs)
-
-        assertEquals(2, updatePrimaryDoctors.doctors.size)
-    }
-
-    @Test
-    fun `update doctors and categorize changes`() {
-        val userID = UserID("test")
-        val existingDoctorID1 = DoctorID("doctor1")
-        val existingDoctorID2 = DoctorID("doctor2")
-        val primaryDoctors = PrimaryDoctors.create(userID, listOf(existingDoctorID1, existingDoctorID2))
-
-        // newDoctorIDsには既存のドクターIDと新しいドクターIDを指定
-        val newDoctorIDs = listOf(existingDoctorID1, DoctorID("doctor3"))
-        val updatePrimaryDoctors = primaryDoctors.updateDoctors(newDoctorIDs)
-
-        assertEquals(2, updatePrimaryDoctors.doctors.size)
+        assertEquals(UUID.fromString("d8bfa31d-54b9-4c64-a499-6c522517e5a0"), primaryDoctor.primaryDoctorID.value)
+        assertEquals("test", primaryDoctor.userID.value)
+        assertEquals("doctor", primaryDoctor.doctorID.value)
     }
 }
